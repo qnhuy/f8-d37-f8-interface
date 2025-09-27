c5f147d364c6e2bfea6e2fece05860a1e4eb0e14
Quang Huy <ngquanghuy.work@gmail.com>
2025-09-28 04:26:54 +0700

    feat: change image url actively

--- src/layouts/DefaultLayout/components/AppLogo/index.jsx
+++ src/layouts/DefaultLayout/components/AppLogo/index.jsx
@@ -3,7 +3,7 @@ import styles from './AppLogo.module.scss'
 
 const AppLogo = function () {
     return <NavLink to={'/'} className={styles.container}>
-        <img className={styles.img} src="./assets/images/f8_icon.png" alt="app logo" />
+        <img className={styles.img} src="https://zoom.fullstack.edu.vn/assets/f8_icon.png" alt="app logo" />
         <p className={styles.slogan}>Học Lập Trình Để Đi Làm</p>
     </NavLink>
 }
--- src/layouts/DefaultLayout/components/Header/components/UserMenu/index.jsx
+++ src/layouts/DefaultLayout/components/Header/components/UserMenu/index.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 
 const user = {
     name: 'Nguyen Quang Huy',
-    avatar: 'https://scontent.fhan5-10.fna.fbcdn.net/v/t39.30808-6/490714068_593610750370041_829296000764001617_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=KEdlQIbniJMQ7kNvwGzk5YQ&_nc_oc=Adl-NVJrAat67ONSFCoqTV5GLkR7TNA72H_knQLDRukp_QHX-iDTqNAKDwOV7UOkKLs&_nc_zt=23&_nc_ht=scontent.fhan5-10.fna&_nc_gid=tw-NmgQbLUcCGZ1ZOAMvDw&oh=00_AfbGAsm0PDGzRxjy1uDUM4DcCMBm8BH5Fpr-mZTLl5fgHg&oe=68D4CCB0',
+    avatar: 'https://files.fullstack.edu.vn/f8-prod/public-images/68d85643b8a70.png',
     email: 'quanghuy@gmail.com'
 }
 
